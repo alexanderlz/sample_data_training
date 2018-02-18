51691b9f8e23afbbd969f9ee74f31ed74dd46544
alexanderlz <alexander.leibzon@gmail.com>
2018-02-18 12:44:11 +0200

    script

--- insert_overwrite_directory.sql
+++ insert_overwrite_directory.sql
@@ -1,1 +1,5 @@
+set hive.io.output.fileformat=CSVTextFile;
+
+insert overwrite local directory '/tmp/CSVrepos/' select * from sample_raw limit 100;
+
 insert overwrite directory '/tmp/test1' select * from sample_raw limit 10;
